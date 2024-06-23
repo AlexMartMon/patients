f395ae6caff291bbf4158df8e7dc2d11a3d34b0f
alejandro martin <alejandro12-11@hotmail.com>
2024-06-23 19:52:12 +0900

    veterinaria

--- src/components/PatientList.tsx
+++ src/components/PatientList.tsx
@@ -1,7 +1,30 @@
-
+import { usePatientStore } from "../storage";
+import PatientDetail from "./PatientDetail";
 
 export default function PatientList() {
+  const patients = usePatientStore((state) => state.patients);
   return (
-    <div>PatientList</div>
-  )
+  <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+    {patients.length ?  (
+      <>
+        <h2 className="font-black text-3xl text-center">Listado de pacientes</h2>
+        <p className="text-xl mt-5 mb-10 text-center">
+          Administra tus pacientes{' '}
+          <span className="text-indigo-600 font-bold">pacientes y citas</span>
+        </p>
+        {patients.map(patient => (
+          <PatientDetail key={patient.id} patient={patient} />
+        ))}
+      </>
+    ) : (
+      <>
+      <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+      <p className="text-xl mt-5 mb-10 text-center">
+        Añade un paciente{" "}
+        
+      </p>
+      </>
+    )}
+  </div>
+  );
 }
